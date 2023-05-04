7950a135c078863bcc313ed7266cbcf315d52373
imransarkerweb <imransarker.web@gmail.com>
2023-05-04 08:44:59 +0600

    package installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import ReactPDF from "@react-pdf/renderer";
 
+import "./App.css";
+import { PDFViewer } from "@react-pdf/renderer";
+import MyDocument from "./components/MakePdf/MakePdf";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <PDFViewer>
+      <MyDocument />
+    </PDFViewer>
+  );
 }
-
-export default App
+ReactDOM.render(<App />, document.getElementById("root"));
+ReactPDF.render(<MyDocument />, `${Downloads}/example.pdf`);
+export default App;
